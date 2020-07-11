1ce82ba3a63d9a46e055461bd421edce71cdc674
josebasierra <joseba.sierra@gmail.com>
2020-07-11 16:55:35 +0200

    Shit merge(Player controller /controllable)

---
+++ Assets/Scripts/BControllable.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
+public class BControllable : MonoBehaviour
+{
+    [SerializeField] float lateralForce;
+    [SerializeField] float jumpForce;
+
+    Rigidbody2D myRigidbody;
+    Collider2D myCollider;
+
+
+    private void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<Collider2D>();
+    }
+
+    
+    // Call following methods on 'FixedUpdate' (physics ticks) :
+
+    public virtual void OnLeftKey()
+    {
+        myRigidbody.AddForce(Vector2.left * lateralForce);
+    }
+
+
+    public virtual void OnRightKey()
+    {
+        myRigidbody.AddForce(Vector2.right * lateralForce);
+    }
+
+    //TODO: check if onGround && !isJumping
+    public virtual void OnJumpKey() 
+    {
+        Vector2 currentPosition = transform.position;
+        float yOffset = myCollider.bounds.extents.y;
+
+        var hitInfo = Physics2D.Raycast(currentPosition, Vector2.down, yOffset + 0.05f);
+
+       
+        if (hitInfo.collider != null)
+        {
+            myRigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+
+    public virtual void OnSpecialKey()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float ControlReach = 1.5f;
+    BControllable controllable;
+
+
+    void Start()
+    {
+        controllable = GetComponent<BControllable>();
+    }
+
+
+    //TODO: Get input in Update method and then process it in FixedUpdate
+    void FixedUpdate()
+    {
+        var horizontalValue = Input.GetAxis("Horizontal");
+
+        if (horizontalValue < 0) controllable.OnLeftKey();
+        if (horizontalValue > 0) controllable.OnRightKey();
+        if (Input.GetButton("Jump")) controllable.OnJumpKey();
+        if (Input.GetButton("Special")) controllable.OnSpecialKey();
+
+
+        var closeControllables = GetCloseControllables();
+        foreach (var closeControllable in closeControllables)
+        {
+            Debug.DrawLine(this.transform.position, closeControllable.transform.position, Color.green);
+        }
+
+
+        //GetClose...
+        // if hit.collider != null && distance ok && click
+        //remove and add playerController...
+
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, ControlReach);
+    }
+
+    // hit = raycast() 
+    // collider.GetComponent<BControllable>
+
+    private IEnumerable<Controllable> GetCloseControllables()
+    {
+        bool InReach(Controllable other)
+        {
+            var distance = (other.transform.position - transform.position).sqrMagnitude;
+            return distance < ControlReach;
+        }
+        //raycast
+        var controllables = FindObjectsOfType<Controllable>()
+            .Where(c => c != this)
+            .Where(InReach);
+
+        return controllables;
+    }
+}

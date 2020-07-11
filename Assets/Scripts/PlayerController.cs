803c6beac7569f8f23a4ecdf1f526c69ec67bf9d
estevem96 <estevemm96@gmail.com>
2020-07-11 20:13:21 +0200

    Shit raycast does not work

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -24,42 +25,62 @@ public class PlayerController : MonoBehaviour
         if (horizontalValue > 0) controllable.OnRightKey();
         if (Input.GetButton("Jump")) controllable.OnJumpKey();
         if (Input.GetButton("Special")) controllable.OnSpecialKey();
-
-
-        var closeControllables = GetCloseControllables();
-        foreach (var closeControllable in closeControllables)
-        {
-            Debug.DrawLine(this.transform.position, closeControllable.transform.position, Color.green);
-        }
-
+        
+        HighlightControllableUnderMouse();
 
         //GetClose...
         // if hit.collider != null && distance ok && click
         //remove and add playerController...
-
     }
 
+    private void HighlightControllableUnderMouse()
+    {
+        var mouseControllable = TryGetControllableOnMousePosition();
+        if (mouseControllable == null) return;
+        
+        Debug.DrawLine(this.transform.position, mouseControllable.transform.position, Color.green);
 
+        Debug.Log($"Highlighting object {mouseControllable.gameObject.name}");
+    }
+    
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(transform.position, ControlReach);
     }
-
-    // hit = raycast() 
-    // collider.GetComponent<BControllable>
-
-    private IEnumerable<Controllable> GetCloseControllables()
+    
+    private BControllable TryGetControllableOnMousePosition()
     {
-        bool InReach(Controllable other)
+        bool InReach(Component other)
         {
             var distance = (other.transform.position - transform.position).sqrMagnitude;
             return distance < ControlReach;
         }
-        //raycast
-        var controllables = FindObjectsOfType<Controllable>()
-            .Where(c => c != this)
-            .Where(InReach);
+        
+        // Try get controllable object under mouse
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        var hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+        if (hit.collider == null)
+        {
+            Debug.Log("no hit");
+            return null;
+        }
 
-        return controllables;
+        if (hit.collider.gameObject.GetComponent<BControllable>() == null)
+        {
+            Debug.Log("Object has no controllable");
+            Debug.Log(hit.collider.gameObject.name);
+            Debug.Log(hit.collider.gameObject);
+            return null;
+        }
+        
+        var controllableUnderMouse = hit.collider.gameObject.GetComponent<BControllable>();
+
+        if (controllableUnderMouse == controllable) return null;
+
+        return InReach(controllableUnderMouse)
+            ? controllableUnderMouse
+            : null;
+
+
     }
 }

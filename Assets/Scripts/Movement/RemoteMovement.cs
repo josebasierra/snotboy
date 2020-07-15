246cbdceea845b37275f6078cb0e7f789484ecd5
estevem96 <estevemm96@gmail.com>
2020-07-15 21:39:27 +0200

    Remote movement

---
+++ Assets/Scripts/Movement/RemoteMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Movement
+{
+    public class RemoteMovement : MonoBehaviour, IMovement
+    {
+        [SerializeField] public GameObject ControlledObject;
+
+        public void Move(Vector2 direction)
+        {
+            if (ControlledObject == null) return;
+
+            var movement = ControlledObject.GetComponent<IMovement>();
+
+            movement?.Move(direction);
+        }
+    }
+}

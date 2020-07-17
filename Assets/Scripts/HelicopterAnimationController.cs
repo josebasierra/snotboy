b1693630c354cb16e95a03f774988f5c0c5780d3
estevem96 <estevemm96@gmail.com>
2020-07-17 18:45:51 +0200

    Added helicopter sprite and animation controller

---
+++ Assets/Scripts/HelicopterAnimationController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utility;
+
+public class HelicopterAnimationController : MonoBehaviour
+{
+    [SerializeField] float VelocityFlipTrigger = 0.5f;
+    
+    private Animator animator;
+    private Collider2D myCollider;
+    private Rigidbody2D myRigidbody;
+    
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        myCollider = GetComponent<Collider2D>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        var xVelocity = myRigidbody.velocity.x;
+
+        if (Math.Abs(xVelocity) > VelocityFlipTrigger)
+        {
+            // reverse object
+            var newScale = transform.localScale;
+            if (Mathf.Sign(newScale.x) != Mathf.Sign(xVelocity)) newScale.x *= -1;
+            transform.localScale = newScale;
+        }
+
+        animator.SetBool("isOnGround", Physics2DUtility.IsOnGround(transform.position, myCollider));
+    }
+}

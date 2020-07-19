e2f96ad9597018bae325f7c5e1674bce404a953d
estevem96 <estevemm96@gmail.com>
2020-07-19 20:58:23 +0200

    Rocket prefab - Heavy box prefab - Made some light modifications to level 1

---
+++ Assets/Scripts/Animation/RocketAnimationController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utility;
+
+namespace Animation
+{
+    public class RocketAnimationController : MonoBehaviour
+    {
+        private Animator animator;
+        private Collider2D myCollider;
+        private Rigidbody2D myRigidbody;
+
+        private void Start()
+        {
+            animator = GetComponent<Animator>();
+            myCollider = GetComponent<Collider2D>();
+            myRigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            var yVelocity = myRigidbody.velocity.y;
+            animator.SetBool("rocketOn", yVelocity > 0);
+        }
+    }
+}

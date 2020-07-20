2860eec0fcf2e33bbeb6461db5e96ccec7f7f51a
estevem96 <estevemm96@gmail.com>
2020-07-20 18:52:11 +0200

    Added tips to Level 0 (tutorial)  - Sign prefab

---
+++ Assets/Scripts/SignController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SignController : MonoBehaviour
+{
+    private Collider2D myCollider;
+    private GameObject myTextObject;
+    private TextMeshPro text;
+
+    private void Start()
+    {
+        myTextObject = transform.Find("SignText").gameObject;
+        myCollider = GetComponent<Collider2D>();
+        text = myTextObject.GetComponent<TextMeshPro>();
+        
+        HideText();
+    }
+
+
+    private void Update()
+    {
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("object entered");
+        if (other.gameObject.CompareTag("Player"))
+        {
+            ShowText();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("object exited");
+        if (other.gameObject.CompareTag("Player"))
+        {
+            HideText();
+        }
+    }
+
+    public void HideText()
+    {
+        text.enabled = false;
+    }
+
+    public void ShowText()
+    {
+        text.enabled = true;
+    }
+}

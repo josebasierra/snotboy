68b00b64bad2b8f94efe204fe081e5ce38de59ef
josebasierra <joseba.sierra@gmail.com>
2020-07-21 16:02:09 +0200

    Physics2DUtility.IsOnGround new implementation.

--- Assets/Scripts/Utility/Utility.cs
+++ Assets/Scripts/Utility/Utility.cs
@@ -6,18 +6,17 @@ namespace Utility
 {
     public static class Physics2DUtility
     {
-
-        //TODO: Pass layer
         public static bool IsOnGround(Vector2 position, Collider2D collider)
         {
-            float xOffset = collider.bounds.extents.x;
-            float yOffset = collider.bounds.extents.y;
+            var contactPoints = new List<ContactPoint2D>();
+            collider.GetContacts(contactPoints);
 
-            var hitDown = Physics2D.Raycast(position + new Vector2(0, -yOffset), Vector2.down, 0.2f);
-            var hitDownLeft = Physics2D.Raycast(position + new Vector2(-xOffset, -yOffset), Vector2.down, 0.2f);
-            var hitDownRight = Physics2D.Raycast(position + new Vector2(xOffset, -yOffset), Vector2.down, 0.2f);
+            foreach (var contact in contactPoints)
+            {
+                if (contact.point.y < position.y) return true;
+            }
 
-            return hitDown.collider != null || hitDownLeft.collider != null || hitDownRight.collider != null;
+            return false;
         }
     }
 }

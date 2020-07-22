9031c3ad756c38b133cabbdb7eab1a6be4b6ea46
josebasierra <joseba.sierra@gmail.com>
2020-07-22 21:04:50 +0200

    Pause during pause menu. Fixed pause menu openig after winning. Pixel camera configured to scale correctly when changing resolution.

---
+++ Assets/Scripts/SetTextSortingLayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetTextSortingLayer : MonoBehaviour
+{
+    public int sortingOrder = 10;
+
+    void Start()
+    {
+        var mRenderer = GetComponent<MeshRenderer>();
+        mRenderer.sortingOrder = sortingOrder;
+    }
+}
